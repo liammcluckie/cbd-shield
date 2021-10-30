0f7f01424798e8bf10efc9828911f7b74406ab0c
liammcluckie <liammcluckie@hotmail.co.uk>
2021-10-30 18:07:17 +0000

    Add category and product classes in models.py

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """ Class for category fields  """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_deferred_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """ Class for product fields """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
