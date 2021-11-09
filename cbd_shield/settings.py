9b7c1677936d6fe4f877a1be4764ed8bd86c1641
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-09 18:59:55 +0000

    Add stripe setting variables to app

--- cbd_shield/settings.py
+++ cbd_shield/settings.py
@@ -182,3 +182,8 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
