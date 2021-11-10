41627b2f9fdfcb16ecf777959acca3bc862019ee
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-10 19:09:08 +0000

    Add checkout success functionality to checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -41,7 +41,7 @@ class Order(models.Model):
         Including delivery costs
         """
         self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total__sum']
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
