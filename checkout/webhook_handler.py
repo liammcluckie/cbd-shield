551977c4c9b844ef16ce6b7dc865d12c52ef08d5
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-13 15:06:15 +0000

    Add stripe handler class for various webhooks

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """ Handle Stripe webhooks """
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """ Handle an error/unknown webhook event """
+
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """ Handle the payment intent webhook event """
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ Handle the payment intent payment failed webhook event """
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
