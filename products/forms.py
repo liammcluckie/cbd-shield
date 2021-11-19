686544671d19790b3cc007f9b8727875d3d2ee35
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-19 12:20:49 +0000

    Fix typo and add class to form in products app

--- products/forms.py
+++ products/forms.py
@@ -2,17 +2,19 @@ from django import forms
 from .models import Product, Category
 
 
-class Productform(forms.ModelForm):
+class ProductForm(forms.ModelForm):
     """ Create product admin form including categories and friendly names """
     
     class Meta:
         model = Product
         fields = '__all__'
     
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_names = [(c.id, c.get_deferred_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'add-product-form-input'
  
