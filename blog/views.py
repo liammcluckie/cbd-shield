7ac9b4fb22258bc35818b47d0fc9baf4de89642b
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-21 16:26:24 +0000

    Add views for all blog and single blog to blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import BlogPost
+
+
+def all_blog_posts(request):
+    blogs = BlogPost.objects.all()
+
+    context = {
+        'blogs': blogs,
+    }
+
+    return render(request, 'blog/blog.html', context)
+
+
+def single_blog(request, blog_id):
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request,'blog/single_blog.html', context)
