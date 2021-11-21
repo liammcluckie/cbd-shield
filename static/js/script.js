c1f6c7b0a53318b45c8a0a1b25579e845b875564
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-21 13:03:46 +0000

    Fix and improve scroll to top anchor function

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,35 @@
-// Add visible class to back to top button when user scrolls past 100px
+/**
+ * Set scrolling variable to false
+ * Add document scroll event listener
+ * Set scrolling variable to true in order to call setInterval to reduce throttling
+ * Add visible class to back to top button when user scrolls past 100px
+ */
 
-window.onscroll = function() {
-    scrollToTop();
-};
+let scrolling = false;
 
-function scrollToTop() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("scroll-to-top").classList.add("visible-btn");
-  } else {
-    document.getElementById("scroll-to-top").classList.remove("visible-btn");
-  }
-}
+document.body.addEventListener('scroll', () => {
+  scrolling = true;
+  setInterval(() => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      scrolling = false;
+      document.getElementById("scroll-to-top").classList.add("visible-btn");
+    } else {
+      document.getElementById("scroll-to-top").classList.remove("visible-btn");
+    }
+  },300);
+});
+
+/**
+ * Add click event listener to scroll to top anchor
+ * Scroll window to top animated smoothly
+ */
+
+document.getElementById('scroll-to-top').addEventListener('click', () => {
+    // For Safari
+    document.body.scrollTo({top: 0, behavior: 'smooth'});
+    // For all other browsers
+    document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
+});
 
 /**
  * Add focus event listener to search input field
