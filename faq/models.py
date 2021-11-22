498a3a711737c9a2e9c3f0adc99e68dc266b7bbb
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-22 19:30:05 +0000

    Add accordion id field to faq model

--- faq/models.py
+++ faq/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Faq(models.Model):
     question = models.TextField()
     answer = models.TextField()
+    item_id_number = models.CharField(default='One', max_length=20, null=False, blank=False)
 
     def __str__(self):
         return self.question
