a0481a7388b9fa36f46bc716b77f3248e37aab39
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-24 19:47:51 +0000

    Fix pylint two blank lines error in reviews admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 from .models import ProductReview
 
+
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         'product',
         'user',
         'stars'
     )
+
 
 admin.site.register(ProductReview, ReviewAdmin)
