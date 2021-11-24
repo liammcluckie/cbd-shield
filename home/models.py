a087fecc2315b294db754a3006b1d9c351dd741c
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-24 19:29:27 +0000

    Add date time field to newsletter model

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import DateTimeField
+
 
 
 class NewsletterRegister(models.Model):
@@ -6,7 +8,8 @@ class NewsletterRegister(models.Model):
     Add user email to newsletter model
     """
     email = models.EmailField(max_length=254, null=False, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         """ Order user emails by date added """
-        ordering = ['-email']
+        ordering = ['-date_added']
