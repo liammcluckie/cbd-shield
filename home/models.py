3f422851f8ac19370f5e5744e0cbef6b72432c1b
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-24 17:59:08 +0000

    Create newsletter register model and add to admin

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterRegister(models.Model):
+    """
+    Add user email to newsletter model
+    """
+    email = models.EmailField(max_length=254, null=False, blank=False)
+
+    class Meta:
+        """ Order user emails by date added """
+        ordering = ['-email']
