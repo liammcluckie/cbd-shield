474cb92539228b0fcc94bd3f8363a52eb2f6f105
liammcluckie <liammcluckie@hotmail.co.uk>
2021-11-24 19:30:07 +0000

    Add view function to register user email to model

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('home/', views.add_user_signup, name='add_user_signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+
+from .models import NewsletterRegister
 
 
 def index(request):
     """ View to return index page """
 
     return render(request, 'home/index.html')
+
+
+def add_user_signup(request):
+    """
+    Add users email to model DB
+    """
+
+    if request.method == 'POST':
+        user_email = request.POST.get('newsletter_signup', '')
+        newsletter = NewsletterRegister.objects.create(
+            email=user_email)
+
+        messages.success(request, 'Thanks for signing up to our newsletter. \
+            Check your inbox soon for our welcome email!')
+
+        return(redirect(reverse('home')))
+    else:
+        messages.error(request, 'Sorry there has been an error! Please \
+            check your email and try again.')
+
+        return(redirect(reverse('home')))
